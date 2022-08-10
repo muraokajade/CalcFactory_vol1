ae083fd1ec230b9312f3700885a4dc82a4dc9374
村岡兼通 <muraokakanemichi@muraokakanemichinoMacBook-Pro.local>
2022-08-10 13:33:04 +0900

    paginate missed

--- app/Http/Controllers/IngController.php
+++ app/Http/Controllers/IngController.php
@@ -9,7 +9,7 @@ class IngController extends Controller
 {
     public function index()
     {
-        $ingredients = Ingredients::pagenate(20);
+        $ingredients = Ingredients::paginate(20);
 
         return view('ingredients.index', compact('ingredients'));
     }
